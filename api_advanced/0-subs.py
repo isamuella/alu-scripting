4a94382c3d2be7de8a0c4f57f06457961bf232c7
isamuella <s.ineza@alustudent.com>
2025-02-21 13:31:28 +0000

    ex0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
 """
-A Query Reddit API for number of subscribers for a given subreddit
+Query Reddit API for number of subscribers for a given subreddit
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-        return number of subscribers for a given subreddit
-        and return 0 if invalid subreddit given
+    Return the number of subscribers for a given subreddit.
+    If subreddit is invalid, return 0.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
+    headers = {
+        'User-Agent': 'MyRedditBot/1.0 (by ALU_student)'
+    }
 
-    r = requests.get(url, headers=headers).json()
-    subscribers = r.get('data', {}).get('subscribers')
-    if not subscribers:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code != 200:
+            return 0
+
+        data = response.json()
+        return data.get('data', {}).get('subscribers', 0)
+
+    except requests.exceptions.RequestException:
         return 0
-    return subscribers
