4f546d096aa271d2a4751cddd7b531f4531cfe10
isamuella <s.ineza@alustudent.com>
2025-02-21 14:37:00 +0000

    ex1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,43 +1,22 @@
 #!/usr/bin/python3
 """
-Query Reddit API for the titles of the first 10 hot posts for a given subreddit
+Query Reddit API for titles of top ten posts of a given subreddit
 """
 import requests
 
 
 def top_ten(subreddit):
     """
-    Prints the titles of the first 10 hot posts for a given subreddit.
-    Prints None if the subreddit is invalid or no posts are found.
+        return top ten titles for a given subreddit
+        return None if invalid subreddit given
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "MyRedditBot/1.0 (by ALU_student)"}
-    params = {"limit": 10}  # Get only the first 10 posts
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
 
-    try:
-        response = requests.get(
-            url, headers=headers, params=params, allow_redirects=False
-        )
-
-        # If subreddit doesn't exist or request fails (not 200 OK), print None
-        if response.status_code != 200:
-            print(None)
-            return
-
-        # Try parsing JSON
-        data = response.json()
-        posts = data.get('data', {}).get('children', [])
-
-        # If no posts are found, print None
-        if not posts:
-            print(None)
-            return
-
-        # Print the top 10 post titles
-        for post in posts:
-            print(post['data']['title'])
-
-    except requests.RequestException:
-        # If network issues or invalid responses, print None
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    r = requests.get(url, headers=headers).json()
+    top_ten = r.get('data', {}).get('children', [])
+    if not top_ten:
         print(None)
-
+    for t in top_ten:
+        print(t.get('data').get('title'))
