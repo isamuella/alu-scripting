f973420eac9085f9c4db4a2905b43bdb044150cb
isamuella <s.ineza@alustudent.com>
2025-02-21 14:16:32 +0000

    ex1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
 """
-function that queries the 'Reddit API'
-then prints the titles of the first 10 hot posts listed for a given subreddit.
+Module to query the Reddit API and print titles of first 10 hot posts
 """
 import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://oauth.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:alu-script:v1.0 (by /u/your_username)"
+    }
+    params = {
+        "limit": 10
+    }
+    
+    response = requests.get(url, headers=headers, params=params,
+                           allow_redirects=False)
+    
+    if response.status_code == 404:
+        print(None)
+        return
+    
     if response.status_code != 200:
         print(None)
         return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+    
+    try:
+        results = response.json().get("data").get("children")
+        for post in results:
+            print(post.get("data").get("title"))
+    except Exception:
+        print(None)
